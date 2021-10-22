82ea99af5c1906b098f1f8e410bfbe7176717873
Mohamed Chawki Cheikh <Publish_Library>
2021-10-22 20:02:14 +0100

    remove pers menu

--- app.py
+++ app.py
@@ -14,6 +14,26 @@ print("suuuuuuuuuuuuuuuuuuuuuuup")
 db = SQLAlchemy(app)
 print("suuuuuuuuuuuuuuuuuuuuuuup222222222222222")
 
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pname = db.Column(db.String(80), unique=True, nullable=False)
+    color = db.Column(db.String(120), nullable=False)
+
+    def __init__(self, pname, color):
+        self.pname = pname
+        self.color = color
+
+
+@app.route("/personadd", methods=['POST'])
+def personadd():
+    pname = request.form["pname"]
+    color = request.form["color"]
+    entry = People(pname, color)
+    db.session.add(entry)
+    db.session.commit()
+
+
 @app.route('/', methods=['GET'])
 def verify():
     # when the endpoint is registered as a webhook, it must echo back
---
+++ models/people.py
@@ -0,0 +1,8 @@
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pname = db.Column(db.String(80), unique=True, nullable=False)
+    color = db.Column(db.String(120), nullable=False)
+
+    def __init__(self, pname, color):
+        self.pname = pname
+        self.color = color
