0154ff42685817e67299589f48242fff49943d52
Mohamed Chawki Cheikh <Publish_Library>
2021-11-10 18:05:22 +0100

    fix

--- app.py
+++ app.py
@@ -564,10 +564,11 @@ def received_postback(event):
         send_text_message(
             sender_id, "Welcome to the Asylex bot - Postback was called")
     else:
-        next_question = Question.query.filter_by(previous_answer_id=int(payload)).first()
-        if  next_question: 
+        next_question = Question.query.filter_by(
+            previous_answer_id=int(payload)).first()
+        if next_question:
             answers = list(map(lambda answer: (answer.answer, answer.id),
-                    Answer.query.filter_by(question_id=next_question.id).all()))
+                               Answer.query.filter_by(question_id=next_question.id).all()))
             send_buttons_message(sender_id, next_question.question, answers)
         # Notify sender that postback was successful
         # send_text_message(sender_id, "Welcome to the Asylex bot - Anything you type will be echoed back to you, except for the following keywords: image, file, audio, video, button, generic, share.")
